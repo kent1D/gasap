1ac74c290a2fdf3d10b4da078e55a64d5e2b64d1
kent1 <kent1@arscenic.info>
2014-05-09 13:26:51 +0200

    Une installation moderne

--- gasap_administrations.php
+++ gasap_administrations.php
@@ -2,40 +2,33 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
+/**
+ * Installation/maj des tables de GASAP
+ *
+ * @param string $nom_meta_base_version
+ * @param string $version_cible
+ */
 function gasap_upgrade($nom_meta_base_version,$version_cible){
-	
-	if ($version_cible > $GLOBALS['meta'][$nom_meta_base_version]){
-		
-		if ($GLOBALS['meta'][$nom_meta_base_version] == ""){
-			echo 'gasap install '.$version_cible;
-			include_spip('base/create');
-			creer_base();
-			maj_tables("spip_gasaps");
-			maj_tables("spip_producteurs");
-			maj_tables("spip_particuliers");
-			maj_tables("spip_gasaps_particuliers");
-			maj_tables("spip_producteurs_gasaps");
-		}else{
-			echo 'gasap '.$GLOBALS['meta'][$nom_meta_base_version].' maj from '.$version_cible;
-			maj_tables("spip_gasaps");
-			maj_tables("spip_producteurs");
-			maj_tables("spip_particuliers");
-			maj_tables("spip_gasaps_particuliers");
-			maj_tables("spip_producteurs_gasaps");
-		}
-		
-	}
-	ecrire_meta($nom_meta_base_version, $current_version=$version_cible);
-	ecrire_metas();
+	$maj = array();
+	$maj['create'] = array(
+		array('maj_tables',array('spip_gasaps,spip_producteurs,spip_particuliers,spip_gasaps_particuliers,spip_producteurs_gasaps'))
+	);
+
+	include_spip('base/upgrade');
+	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
+/**
+ * Desinstallation/suppression des tables GASAP
+ *
+ * @param string $nom_meta_base_version
+ */
 function gasap_vider_tables($nom_meta_base_version){
 	sql_drop_table("spip_gasaps");
 	sql_drop_table("spip_producteurs");
 	sql_drop_table("spip_particuliers");
 	sql_drop_table("spip_gasaps_particuliers");
 	sql_drop_table("spip_producteurs_gasaps");
-	effacer_meta('gasapdb_version');
 	effacer_meta($nom_meta_base_version);
 }
 
