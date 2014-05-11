98057ae01da5cb157cc04bfacb9f225b8a647aca
kent1 <kent1@arscenic.info>
2014-05-11 19:07:25 +0200

    Le fichier de langue de l'objet "Particulier"

---
+++ lang/particulier_fr.php
@@ -0,0 +1,44 @@
+<?php
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Produit automatiquement par le plugin LangOnet a partir de la langue source fr
+// Module: gasap
+// Langue: fr
+// Date: 01-03-2011 01:57:31
+// Items: 123
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// E
+	'editer_particulier' => 'éditer le particulier',
+	'editer_un_particulier' => 'Editer un particulier',
+	'entree_composition_menage' => 'Vous cherchez un panier pour combien de personnes ?',
+	'entree_lieu' => 'Une petite description : ',
+	'entree_nombre_menage' => 'Entrez ici le nombre de personne que compose votre ménage',
+	'entree_permanence' => 'Indiquez ici les permanences de votre groupe.',
+	'entree_personne_contacte' => 'Voulez-vous &ecirc;tre la personne de contact?',
+	'entree_personne_de_contacte' => 'Voulez-vous &ecirc;tre la personne de contact?',
+	'entree_remarques' => 'Indiquez ici les éventuelles remarques concernant le groupe.',
+	'est_une_personne_de_contacte' => 'Cette personne est une personne de contacte.',
+	'etes_vous_sur_de_vouloir_supprimer_ce_gasap' => 'Etes-vous s&ucirc;r de vouloir supprimer ce GASAP ?',
+
+// G
+	'particulier_numero' => 'Numéro',
+	'particuliers' => 'Les particuliers',
+
+// I
+	'icone_creer_particulier' => 'Ajouter un particulier',
+	'icone_modifier_particulier' => 'Modifier le particulier',
+	'info_1_particulier' => '1 particulier',
+	'info_nb_particuliers' => '@nb@ particuliers',
+
+// M
+
+	// T
+	'titre_logo_particulier' => 'Logo du particulier',
+	'titre_particulier' => 'Particulier',
+	'titre_particuliers' => 'Les particuliers',
+
+);
+?>
