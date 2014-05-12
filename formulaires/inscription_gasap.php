7e3c06a5938efc46f8d9e026009f989f707686dc
kent1 <kent1@arscenic.info>
2014-05-12 09:45:01 +0200

    On revoit le HTML du formulaire d'inscription d'un GASAP
    On revoit également l'ensemble du PHP que l'on reprend sur celui
    d'édition de GASAP dans le privé

--- formulaires/inscription_gasap.php
+++ formulaires/inscription_gasap.php
@@ -2,132 +2,81 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-function formulaires_inscription_gasap_charger_dist(){
+include_spip('formulaires/editer_gasap');
 
-	$valeurs = Array();
-	
-	$valeurs["new"] = "oui";
-	
-	
-	(_request('nom')?$valeurs['nom'] = _request('nom'):$valeurs['nom'] =$valeurs['nom']);
-	(_request('adresse')?$valeurs['adresse'] = _request('adresse'):$valeurs['adresse'] =$valeurs['adresse']);
-	(_request('numero')?$valeurs['numero'] = _request('numero'):$valeurs['numero'] =$valeurs['numero']);
-	(_request('code_postal')?$valeurs['code_postal'] = _request('code_postal'):$valeurs['code_postal'] =$valeurs['code_postal']);
-	(_request('ville')?$valeurs['ville'] = _request('ville'):$valeurs['ville'] =$valeurs['ville']);
-	(_request('pays')?$valeurs['pays'] = _request('pays'):$valeurs['pays'] =$valeurs['pays']);
-	(_request('telephone')?$valeurs['telephone'] = _request('telephone'):$valeurs['telephone'] =$valeurs['telephone']);
-	(_request('fax')?$valeurs['fax'] = _request('fax'):$valeurs['fax'] =$valeurs['fax']);
-	(_request('gsm')?$valeurs['gsm'] = _request('gsm'):$valeurs['gsm'] =$valeurs['gsm']);
-	(_request('email')?$valeurs['email'] = _request('email'):$valeurs['email'] =$valeurs['email']);
-	(_request('permanance')?$valeurs['permanance'] = _request('permanance'):$valeurs['permanance'] =$valeurs['permanance']);
-	(_request('remarques')?$valeurs['remarques'] = _request('remarques'):$valeurs['remarques'] =$valeurs['remarques']);
-	
-	
+function formulaires_inscription_gasap_charger_dist($id_gasap='new', $retour='', $config_fonc='gasaps_edit_config', $row=array(), $hidden=''){
+	$valeurs = array();
+
+	$valeurs = formulaires_editer_objet_charger('gasap',$id_gasap,'','',$retour,$config_fonc,$row,$hidden);
+
 	return $valeurs;
-	
 }
 
-function formulaires_inscription_gasap_verifier_dist(){
+function formulaires_inscription_gasap_verifier_dist($id_gasap='new', $retour='', $config_fonc='gasaps_edit_config', $row=array(), $hidden=''){
 
-	$erreurs = Array();
-	
+	$erreurs = array();
+
 	include_spip('inc/filtres');
-	
+
 	if (_request('email') AND !email_valide(_request('email')))
 		$erreurs['email'] = _T('gasap:ce_mail_n_est_pas_valide');
-		
-	if (_request('bfg_god_mode')){
+
+	if (_request('bfg_god_mode'))
 		$erreurs['bfg_god_mode'] = "Nikouuuuuz !!";
-		
-	}
 	
-	if (count($erreurs)) {
+	if (count($erreurs))
 		$erreurs['message_erreur'] = "Une erreur est présente dans votre saisie";
-	}
-	
+
 	return $erreurs;
 	
 }
 
-function formulaires_inscription_gasap_traiter_dist(){
-	include_spip("base/abstract_sql");
-	$valeurs = Array();
+function formulaires_inscription_gasap_traiter_dist($id_gasap='new', $retour='', $config_fonc='gasaps_edit_config', $row=array(), $hidden=''){
+	/**
+	 * Si on n'a pas de latitude ni ou de longitude, on lance le géocoder
+	 */
+	if(!_request('lat') OR !_request('lng')){
+		include_spip('inc/distant');
+		$adresse = _request('numero').' '._request('adresse').', '._request('ville');
+		$url = 'http://nominatim.openstreetmap.org/search';
+		$url = parametre_url(parametre_url(parametre_url(parametre_url($url,'format','json','&'),'limit',1,'&'),'addressdetails',1,'&'),'q',$adresse,'&');
+		$geocoder = json_decode(recuperer_page($url),true);
+		if(is_array($geocoder) && is_array($geocoder[0])){
+			set_request('lat',$geocoder[0]['lat']);
+			set_request('lng',$geocoder[0]['lon']);
+		}
+	}
+	set_request('statut','valide');
+	$res = formulaires_editer_objet_traiter('gasap',$id_gasap,'','',$retour,$config_fonc,$row,$hidden);
 	
-	// On charge les champs qu'on a de toute facon besoin.
-	$new = "oui";
-	$valeurs['id_gasap'] = _request("id_gasap");
-	$valeurs['maj'] = date("Y-m-d G:i:s");
-	
-	
-	/* 
-	 * ici on viens charger les différents champs de l'gasaps
-	 * du genre :
-	 * $valeurs['titre'] = _request("titre");
-	 * $valeurs['texte'] = _request("texte");
-	 * $valeurs['zorglub'] = _request("zorglub");
-	 * ...
-	 * 
-	 * !Attention!:
-	 * On ne 'charge' ici que les element non systeme.
-	 * On entend par element systeme  les id_auteur du createur,
-	 * la date de creation et de modification de l'gasap, etc...
-	 * On ne charge ici que son titre, son texte, etc...
-	 * 
-	 * */
-	
-	$valeurs['nom'] = _request('nom');
-	$valeurs['adresse'] = _request('adresse');
-	$valeurs['numero'] = _request('numero');
-	$valeurs['code_postal'] = _request('code_postal');
-	$valeurs['ville'] = _request('ville');
-	$valeurs['pays'] = _request('pays');
-	$valeurs['telephone'] = _request('telephone');
-	$valeurs['fax'] = _request('fax');
-	$valeurs['gsm'] = _request('gsm');
-	$valeurs['email'] = _request('email');
-	$valeurs['permanance'] = _request('permanance');
-	$valeurs['remarques'] = _request('remarques');
-
-	$valeurs['statut'] = "valide";
-	$valeurs['id_gasap'] = sql_insertq("spip_gasaps",$valeurs);
-	
-	if ($valeurs['id_gasap'] > 0){
+	if (intval($res['id_gasap']) > 0){
 		$valeurs['message_ok'] = _T('gasap:votre_inscription_a_reussi_un_organisateur_vas_vous_contacter');
 		$envoyer_mail = charger_fonction('envoyer_mail', 'inc');
 		$corps = "Inscription d'un GASAP.
 
 Coordonnee:
 ------------
-nom: ".$valeurs['nom']."
-adresse: ".$valeurs['adresse']."
-numero: ".$valeurs['numero']."
-code_postal: ".$valeurs['code_postal']."
-ville: ".$valeurs['ville']."
-pays: ".$valeurs['pays']."
-Téléphone: ".$valeurs['telephone']."
-Fax : ".$valeurs['fax']."
-Gsm : ".$valeurs['gsm']."
-E-Mail: ".$valeurs['email']."
+nom: "._request('nom')."
+adresse: "._request('adresse')."
+numero: "._request('numero')."
+code_postal: "._request('code_postal')."
+ville: "._request('ville')."
+pays: "._request('pays')."
+Téléphone: "._request('telephone')."
+Fax : "._request('fax')."
+Gsm : "._request('gsm')."
+E-Mail: "._request('email')."
 
 Pour plus d'info, l'inscription est reprise dans le partie privée du site.
 ";
-include_spip('inc/geocoder');
 		$envoyer_mail(lire_config('email_webmaster'), "Inscription d'un GASAP", $corps);
-		$coordonnees = getCoordonnees($valeurs["adresse"]." ".$valeurs["numero"].", ".$valeurs["code_postal"]." ".$valeurs["ville"].", ".$valeurs["pays"]);
-		spip_log("Update lat lng de ".$valeurs["id_gasap"],"gasap");
-		sql_updateq("spip_gasaps",Array("lat" => $coordonnees[2],"lng" => $coordonnees[3],"maj" => date("Y-m-d G:i:s")),Array("id_gasap = ".$valeurs["id_gasap"]));
-		
 	}else{
-		
-		// ca a foire, on remet new parce l'ajout n'a pas marché
-		$valeurs["new"] = _request("new");
 		// on dis que ca n'a pas marché
 		$valeurs['message_erreur'] = _T('gasap:votre_inscription_a_echoue_veuillez_reeseyer_plus_tard');
-		
 	}
 	
 	return $valeurs;
-	
+
 }
 
 ?>
