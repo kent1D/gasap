faecd0ed0a0b7dfd1a024f3ba250fd4284606505
kent1 <kent1@arscenic.info>
2014-05-12 08:54:50 +0200

    On revoit le HTML du formulaire de contact d'un GASAP
    On améliore également toute la partie PHP
    
    Les champs sont tous réellement obligatoires
    
    Il ne retourne rien si le gasap n'a pas d'email
    
    On doit donner comme premier argument du formulaire l'id_gasap

--- formulaires/contact_gasap.php
+++ formulaires/contact_gasap.php
@@ -2,29 +2,40 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-function formulaires_contact_gasap_charger_dist(){
-	$valeurs = Array();
-	(_request('nom')?$valeurs['nom'] = _request('nom'):$valeurs['nom'] =$valeurs['nom']);
-	(_request('email')?$valeurs['email'] = _request('email'):$valeurs['email'] =$valeurs['email']);
-	(_request('texte')?$valeurs['texte'] = _request('texte'):$valeurs['texte'] =$valeurs['texte']);
-	include_spip('inc/filtres');
-	$email = sql_getfetsel("email","spip_gasaps",Array("id_gasap = "._request('id_gasap')));
+function formulaires_contact_gasap_charger_dist($id_gasap){
+	$valeurs = array();
+	$email = sql_getfetsel("email","spip_gasaps","id_gasap = ".intval($id_gasap));
 	
-	if(!email_valide($email)){
-		return false;
+	/**
+	 * Si le GASAP n'a pas d'email ou que le paramètre n'est pas un nombre
+	 * Le formulaire n'est pas éditable
+	 */
+	if(!is_numeric($id_gasap) OR !$email){
+		$valeurs['editable'] = false;
+		return $valeurs;
 	}
+
+	foreach(array('nom','email','texte') as $champ){
+		$valeurs[$champ] = _request($champ);
+	}
+
 	return $valeurs;
-	
 }
 
-function formulaires_contact_gasap_verifier_dist(){
-	$erreurs = Array();
+function formulaires_contact_gasap_verifier_dist($id_gasap){
+	$erreurs = array();
 	
 	include_spip('inc/filtres');
 	
-	if (_request('email') AND !email_valide(_request('email')))
-		$erreurs['email'] = _T('gasap:ce_mail_n_est_pas_valide');
+	foreach(array('email','texte','email') as $champ){
+		if(!_request($champ) OR strlen(_request($champ)) <  2){
+			$erreurs[$champ] = _T('info_obligatoire');
+		}
+	}
 	
+	if (!isset($erreurs['email']) && _request('email') && !email_valide(_request('email')))
+		$erreurs['email'] = _T('gasap:ce_mail_n_est_pas_valide');
+
 	if (count($erreurs))
 		$erreurs['message_erreur'] = _T('gasap:votre_saisie_contient_des_erreurs');
 	
@@ -32,20 +43,19 @@ function formulaires_contact_gasap_verifier_dist(){
 	
 }
 
-function formulaires_contact_gasap_traiter_dist(){
-	include_spip("base/abstract_sql");
-	$valeurs = Array();
+function formulaires_contact_gasap_traiter_dist($id_gasap){
+	$valeurs = array();
 	
-	(_request('nom')?$valeurs['nom'] = _request('nom'):$valeurs['nom'] =$valeurs['nom']);
-	(_request('email')?$valeurs['email'] = _request('email'):$valeurs['email'] =$valeurs['email']);
-	(_request('texte')?$valeurs['texte'] = _request('texte'):$valeurs['texte'] =$valeurs['texte']);
+	foreach(array('nom','email','texte') as $champ){
+		$valeurs[$champ] = _request($champ);
+	}
 	
 	$corps = _T('gasap:message_contact_asso_intro') . "\n" .
 _T('gasap:message_contact_asso_nom') . $valeurs['nom'] . "\n" .
 _T('gasap:message_contact_asso_adresse') . $valeurs['email'] . "\n" .
 _T('gasap:message_contact_asso_message') . $valeurs['texte'];
 	
-$email = sql_getfetsel("email","spip_gasaps",Array("id_gasap = "._request('id_gasap')));
+	$email = sql_getfetsel("email","spip_gasaps","id_gasap = ".intval($id_gasap));
 
 	$envoyer_mail = charger_fonction('envoyer_mail', 'inc');
 	$envoyer_mail($email, _T('gasap:message_contact_asso_sujet'), $corps, $from = "$reply");
